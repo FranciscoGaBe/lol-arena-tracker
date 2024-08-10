ef37e8c596f4f830454a06fa9821703d7d57f84a
Francisco Garrido <francisco.garrido.bear@gmail.com>
2024-08-10 23:45:34 +0200

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import TopButton from './components/TopButton';
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Outlet />
       <TopButton />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className="sticky bottom-0 border-t border-cyan-500 bg-cyan-700 p-4 text-white shadow">
+      <a
+        href="https://github.com/FranciscoGaBe/lol-arena-tracker"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
